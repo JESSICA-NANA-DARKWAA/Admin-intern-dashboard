f7aeea758dc1aace89c04b5fa34bd986f2e425cf
JESSICA-NANA-DARKWAA <jessica.darkwaa@amalitech.org>
2023-12-19 17:45:15 +0000

    router import fixes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loginpage from "../pages/loginpage/Loginpage";
 import Interns from "../pages/Interns/Interns";
-// import Dashboard from "../pages/dashboard/Dashboard";
 import Assessment from "../pages/Assesment/Assessment";
+import Dashboard from "../pages/dashboard/Dashboard";
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Loginpage />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/interns" element={<Interns />} />
         <Route path="/assessment" element={<Assessment />} />
       </Routes>
