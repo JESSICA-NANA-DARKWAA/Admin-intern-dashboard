0be49a36c06e0c8ad6d22c794a289fb6e9d8a436
FrankSam18 <82455910+FrankSam18@users.noreply.github.com>
2023-12-21 09:15:37 +0000

    modaln css, simplified use context for dashboard

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -30,7 +30,7 @@ const AppContextProvider = ({ children }) => {
           action.payload.location === "Takoradi"
             ? state.data.totalNumberInTakoradi + 1
             : state.data.totalNumberInTakoradi;
-
+  console.log({newIntern,newInterns,newTotalNumberInAccra,newTotalNumberInTakoradi,newTotalNumberOfInterns})
         return {
           ...state,
           data: {
